66a5374d2a6c7563999dff1ab273422de4977128
skaddie <87253680+skaddie@users.noreply.github.com>
2022-02-03 03:14:09 +0800

    establishing remote connection

--- config/config.php
+++ config/config.php
@@ -1,7 +1,13 @@
 <?php
 
+// LOCAL CONNECTION
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'db_admin');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'db_admin');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+// REMOTE CONNECTION
+define('DB_HOST', 'remotemysql.com');
+define('DB_NAME', '6DRA0oR09e');
+define('DB_USER', '6DRA0oR09e');
+define('DB_PASS', 'vQb5k5Z6hU');
